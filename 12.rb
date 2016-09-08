2507cef5a68da5a3ed3959a7ba0d1cecfde0c77b
jinnudeveloper <jinnudeveloper@gmail.com>
2016-09-08 16:38:38 +0530

    String array sorting

--- 12.rb
+++ 12.rb
@@ -2,3 +2,8 @@ num_array = [6,3,7,1,0]
 num_array.sort!
 
 num_array.each {|i| puts i}
+
+string_array = ["Charlie","War","Utopia","Augustin"]
+string_array.sort!
+
+string_array.each {|string| puts string}
