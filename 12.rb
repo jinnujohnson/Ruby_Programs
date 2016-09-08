3830637bce6b14c3f4ccf3718219b6571c046392
jinnudeveloper <jinnudeveloper@gmail.com>
2016-09-08 16:42:48 +0530

    Combined comparison operator used

--- 12.rb
+++ 12.rb
@@ -7,3 +7,9 @@ string_array = ["Charlie","War","Utopia","Augustin"]
 string_array.sort!
 
 string_array.each {|string| puts string}
+
+book_1 = "A wrinkle in time"
+book_2 = "A brief history of time"
+
+puts book_1 <=> book_2
+
