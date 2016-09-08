6f1891b230f96deba8d697390770fa7d22bf1821
jinnudeveloper <jinnudeveloper@gmail.com>
2016-09-08 17:13:34 +0530

    Sorting method

---
+++ 13.rb
@@ -0,0 +1,13 @@
+def sorting(arr, rev=false)
+  if rev
+    arr.sort { |item1, item2| item2 <=> item1 }
+  else
+    arr.sort { |item1, item2| item1 <=> item2 }
+  end
+end
+
+books = ["Heart of Darkness", "Code Complete", "The Lorax", "The Prophet", "Absalom, Absalom!"]
+
+puts "A-Z: #{sorting(books)}"
+puts "Z-A: #{sorting(books, true)}"
+
