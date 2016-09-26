0e6ba3416ac48c484e7921870753dcf311b85ff4
jinnudeveloper <jinnudeveloper@gmail.com>
2016-09-26 20:43:46 +0530

    Proc

--- 18.rb
+++ 18.rb
@@ -6,3 +6,12 @@ def yield_name(name)
   puts "Block complete"
 end
 yield_name("Ammu") {|n| puts "My name is #{n}"}
+
+
+def greeter
+  yield
+  puts "Hai dear"
+end
+greeter { puts "Hello you there!" }
+phrase = Proc.new { puts "Hello How are you?" }
+greeter(&phrase)
