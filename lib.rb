0d5e012de7938533b1ae0754c78a5fdac9f82e08
jinnudeveloper <jinnudeveloper@gmail.com>
2016-10-04 12:25:02 +0530

    Library Program Modified

--- lib.rb
+++ lib.rb
@@ -1,17 +1,17 @@
 class Library
-    attr_reader :books
+  attr_reader :books
 
-    def initialize
-        @books = []
-    end
+  def initialize
+    @books = []
+  end
 
-    def add_book(book)
-        @books << book unless @books.include?(book)
-    end
+  def add_book(book)
+    @books << book unless @books.include?(book)
+  end
 
-    def each
-        @books.each { |x| puts x } unless @books.empty?
-    end
+  def each
+    @books.each { |book| puts book } unless @books.empty?
+  end
 end
 
 lib = Library.new
