0a51c8fc0a3f29ec90c72216a281dabc97ba2e90
jinnudeveloper <jinnudeveloper@gmail.com>
2016-10-04 12:16:27 +0530

    Library Pgm

---
+++ lib.rb
@@ -0,0 +1,21 @@
+class Library
+    attr_reader :books
+
+    def initialize
+        @books = []
+    end
+
+    def add_book(book)
+        @books << book unless @books.include?(book)
+    end
+
+    def each
+        @books.each { |x| puts x } unless @books.empty?
+    end
+end
+
+lib = Library.new
+lib.add_book('Book1')
+lib.add_book('Book2')
+puts lib.books
+lib.each
