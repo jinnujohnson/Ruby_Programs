bf5d27bb2fa833264da5cfb45b577a7cfa9bd55c
jinnudeveloper <jinnudeveloper@gmail.com>
2016-11-11 15:21:55 +0530

    Array Inject using class

--- inject.rb
+++ inject.rb
@@ -1,3 +1,15 @@
-a = [1,2,3,4,5]
-p "sum is " + "#{a.inject(0) {|sum, x| sum += x } }"
-p "product is " + "#{a.inject(1) {|pro, x| pro *= x} }"
+class Array_inject
+  def initialize
+    @a = [1,2,3,4,5]
+  end
+  def sum
+    "sum is " + "#{@a.inject(0) {|sum, x| sum += x } }"
+  end
+  def product
+    "product is " + "#{@a.inject(1) {|pro, x| pro *= x} }"
+  end
+end
+
+inj = Array_inject.new
+p inj.sum
+p inj.product
